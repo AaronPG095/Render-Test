62a48ce74c4af7f6eaae811d74792f4d00872fd3
Aaron Paul Greyling <aaron.p.greyling@gmail>
2022-11-10 11:19:47 +0100

    ready for deploy

--- server.js
+++ server.js
@@ -1,50 +1,21 @@
-// the package
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import { auth, errorHandler, notFound } from "./middleware/index.js";
-// import controllers
-
-import {
-  addUser,
-  deleteUserById,
-  getAllUsers,
-  getUserById,
-  updateUserById,
-} from "./controllers/users.controller.js";
-
-// call the express
+import userRouter from "./router/userRouter.js";
+import { errorHandler } from "./middleware/index.js";
 const app = express();
-dotenv.config();
-// the middleware
-
 app.use(cors());
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
-const PORT = process.env.PORT || 5001;
-console.log(process.env.DB_URL);
-// the routers
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  console.log("my first request");
-  res.send("my first request");
+  res.send("hello there");
 });
+app.use("/users", userRouter);
 
-//! without router
-// app.get("/users/all", auth, getAllUsers);
-// app.get("/users/:id", getUserById);
-// app.post("/users/add", addUser);
-// app.delete("/users/:id", deleteUserById);
-// app.put("/users/:id", updateUserById);
-//! with router
-app.use("/users", usersRouter);
-
-// page not found middleware
-
-app.use(notFound);
-// middleware error
 app.use(errorHandler);
-// listen on port
 app.listen(PORT, () => {
-  console.log("the server is ready and listen to port ", PORT);
+  console.log(" 😀 the server is run on port " + PORT);
 });
